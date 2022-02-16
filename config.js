b56073629526ee9d393484311f3934083ab94ffc
r-m-dakshiin <79435405+r-m-dakshiin@users.noreply.github.com>
2022-02-16 18:57:25 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDNRy0NcSI4VjqW5g4aTZ7RemvL6cO86dg",
+    authDomain: "storytellingapp-9fb61.firebaseapp.com",
+    databaseURL: "https://storytellingapp-9fb61-default-rtdb.firebaseio.com",
+    projectId: "storytellingapp-9fb61",
+    storageBucket: "storytellingapp-9fb61.appspot.com",
+    messagingSenderId: "752245879203",
+    appId: "1:752245879203:web:74819ff0b4027bbb00ea16"
+  };
+  
+  // Initialize Firebase
+  //const app = initializeApp(firebaseConfig);
